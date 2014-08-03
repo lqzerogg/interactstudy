af4ec60a1d4f2e7f201078623b99193536a52a60
luochuangjie <luochuangjie@meituan.com>
2014-08-03 10:09:34 +0800

    修改back_password

--- routes/ajax/get_code.js
+++ routes/ajax/get_code.js
@@ -1,9 +1,15 @@
 
 /* GET home page. */
+//ajax接口
+var result = {
+	//获取验证码图片
+	"result": true,
+	"src": "xxxx"
+}
 var exports = {
 	key: '/getCode',
 	response: function(req, res) {
-		var obj = {src: '/img/ss1.png'};
+		var obj = {src: '/img/ss1.png', result: true};
 
 		res.set('Content-Type', 'text/json');
 
--- routes/ajax/send_mail.js
+++ routes/ajax/send_mail.js
@@ -1,3 +1,10 @@
+//ajax接口
+var result = {
+	//发送邮箱验证， 参数有两个，第一个是邮箱地址mail, 第二个是验证码code
+	"result": true,
+	"url": "xxxx",       //转跳地址
+	"errDesc": "xxxx"    //错误描述
+}
 
 /* GET home page. */
 var exports = {
